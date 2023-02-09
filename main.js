560c680b47a608afc5876a9a466a593dcd5729d8
SimoLomba53 <simolomba253@gmail.com>
2023-02-09 15:05:09 +0100

    esercizio completo di bonus

---
+++ main.js
@@ -0,0 +1,23 @@
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message: 'Buon Pomeriggio Vue!'
+        }
+
+    }
+}).mount('#app')
+
+image: ["https://assets2.razerzone.com/images/pnx.assets/618c0b65424070a1017a7168ea1b6337/razer-wallpapers-page-hero-mobile.jpg"]
+
+
+createApp({
+    data() {
+        return {
+            image: ["https://assets2.razerzone.com/images/pnx.assets/618c0b65424070a1017a7168ea1b6337/razer-wallpapers-page-hero-mobile.jpg"]
+        }
+
+    }
+}).mount('#img')
